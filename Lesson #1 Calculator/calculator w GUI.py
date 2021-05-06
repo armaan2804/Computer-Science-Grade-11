e4924e07e982201c8d1a1972711e2f9655001b9a
armaan2804 <873781@pdsb.net>
2021-05-06 00:25:34 -0400

    Update calculator w GUI.py

--- Lesson #1 Calculator/calculator w GUI.py
+++ Lesson #1 Calculator/calculator w GUI.py
@@ -1,10 +1,14 @@
-from tkfrom tkinter import *
+from tkinter import *
+from tkinter import messagebox
+
+messagebox.showinfo("REMINDER", "This calculator expects the first number, an operation, second number, and then equal sign to output answer")
 
 root = Tk()
 root.title("Calculator")
 
-e = Entry(root,width=30, borderwidth=5)
-e.grid(row=1,column=1,columnspan=3, padx=10,pady=10)
+
+e = Entry(root,width=55, borderwidth=5)
+e.grid(row=1,column=1,columnspan=7, padx=10,pady=10)
 
 #e.insert(0,"")
 
@@ -19,6 +23,8 @@ def button_clear():
 def button_add():
   first_number = e.get()
   global f_num
+  global math
+  math ="addition"
   f_num = int(first_number)
   e.delete(0,END)
 
@@ -26,25 +32,72 @@ def button_add():
 def button_equal():
   second_number = e.get()
   e.delete(0,END)
-  e.insert(0,f_num + int(second_number))
+  if math == "addition":
+    e.insert(0,f_num + int(second_number))
+  elif math == "subtraction":
+    e.insert(0,f_num - int(second_number))
+  elif math == "multiplication":
+    e.insert(0,f_num * int(second_number))
+  elif math == "division":
+    e.insert(0,f_num/int(second_number))
+  elif math == "exponent":
+    e.insert(0,f_num**int(second_number))
+
+
+def button_subtract():
+  first_number = e.get()
+  global f_num
+  global math
+  math ="subtraction"
+  f_num = int(first_number)
+  e.delete(0,END)
+
+def button_multiply():
+  first_number = e.get()
+  global f_num
+  global math
+  math ="multiplication"
+  f_num = int(first_number)
+  e.delete(0,END)
+
+def button_divide():
+  first_number = e.get()
+  global f_num
+  global math
+  math ="division"
+  f_num = int(first_number)
+  e.delete(0,END)
+
+def button_exponent():
+  first_number = e.get()
+  global f_num
+  global math
+  math ="exponent"
+  f_num = int(first_number)
+  e.delete(0,END)
 
 
 
-button_1 = Button(root,text="1", padx=40, pady=20, command=lambda:button_click(1))
-button_2 = Button(root,text="2", padx=40, pady=20, command=lambda:button_click(2))
-button_3 = Button(root,text="3", padx=40, pady=20, command=lambda:button_click(3))
-button_4 = Button(root,text="4", padx=40, pady=20, command=lambda:button_click(4))
-button_5 = Button(root,text="5", padx=40, pady=20, command=lambda:button_click(5))
-button_6 = Button(root,text="6", padx=40, pady=20, command=lambda:button_click(6))
-button_7 = Button(root,text="7", padx=40, pady=20, command=lambda:button_click(7))
-button_8 = Button(root,text="8", padx=40, pady=20, command=lambda:button_click(8))
-button_9 = Button(root,text="9", padx=40, pady=20, command=lambda:button_click(9))
-button_0 = Button(root,text="0", padx=40, pady=20, command=lambda:button_click(0))
+button_1 = Button(root,text="1", padx=100, pady=20, command=lambda:button_click(1))
+button_2 = Button(root,text="2", padx=100, pady=20, command=lambda:button_click(2))
+button_3 = Button(root,text="3", padx=100, pady=20, command=lambda:button_click(3))
+button_4 = Button(root,text="4", padx=100, pady=20, command=lambda:button_click(4))
+button_5 = Button(root,text="5", padx=100, pady=20, command=lambda:button_click(5))
+button_6 = Button(root,text="6", padx=100, pady=20, command=lambda:button_click(6))
+button_7 = Button(root,text="7", padx=100, pady=20, command=lambda:button_click(7))
+button_8 = Button(root,text="8", padx=100, pady=20, command=lambda:button_click(8))
+button_9 = Button(root,text="9", padx=100, pady=20, command=lambda:button_click(9))
+button_0 = Button(root,text="0", padx=100, pady=20, command=lambda:button_click(0))
 
 
-button_equal = Button(root,text="=",padx=40,pady=20,command=button_equal)
-button_clear = Button(root,text="clear", padx=30, pady=20, command=button_clear)
-button_add= Button(root,text="+",padx=40,pady=20,command=button_add)
+button_equal = Button(root,text="=",padx=100,pady=20,command=button_equal)
+button_clear = Button(root,text="clear", padx=87, pady=20, command=button_clear)
+button_add= Button(root,text="+(Add)",padx=80,pady=20,command=button_add)
+
+button_subtract = Button(root,text="-(Subtract)",padx=70,pady=20,command=button_subtract)
+button_multiply= Button(root,text="*(Multiply)",padx=67,pady=20,command=button_multiply)
+button_divide= Button(root,text="/(Divide)",padx=75,pady=20,command=button_divide)
+button_exponent=Button(root,text="Exponent", padx=75,pady=20,command=button_exponent)
 
 
 #Put the buttons on the screen
@@ -62,9 +115,14 @@ button_8.grid(row=4,column=2)
 button_9.grid(row=4,column=3)
 
 button_0.grid(row=5,column=1)
-button_equal.grid(row=5,column=3)
-button_clear.grid(row=5,column=2)
-button_add.grid(row=6,column=3)
+button_equal.grid(row=7,column=3)
+button_clear.grid(row=7,column=2)
+button_add.grid(row=5,column=2)
+
+button_subtract.grid(row=5,column=3)
+button_multiply.grid(row=6,column=2)
+button_divide.grid(row=6,column=1)
+button_exponent.grid(row=6,column=3)
 
 
 #if functions when the buttons are printed
@@ -91,5 +149,5 @@ if operation == "-":
   print("Your number is:" + str(x-y))
 
 if operation == "**":
-  print("Your number is:" + str(x**y))inter import *
+  print("Your number is:" + str(x**y))
 
